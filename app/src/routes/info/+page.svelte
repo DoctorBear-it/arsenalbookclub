2e65fcf7ef190ccf043580342a24f50f6011c984
DoctorBear-it <DoctorBear-it@users.noreply.github.com>
2024-07-09 00:50:23 -0400

    Add flowbite-svelte and verify by test banner on Home page.  Implement table on Info page, add UI library links in table.

--- app/src/routes/info/+page.svelte
+++ app/src/routes/info/+page.svelte
@@ -1,5 +1,17 @@
 <script lang="ts"> 
     // @ts-check
+    import {
+    Table,
+    TableBody,
+    TableBodyCell,
+    TableBodyRow,
+    TableHead,
+    TableHeadCell,
+    ImagePlaceholder,
+    Modal
+  } from 'flowbite-svelte';
+  import { slide } from 'svelte/transition';
+
     export let data;
 
     //Don't do this, make these calls in +page.ts or +page.server.ts
@@ -41,8 +53,32 @@
         {id:"Code Examples", url : "https://svelte.dev/examples/each-blocks"},
         {id:"Code Examples", url : "https://svelte.dev/repl/9fb792271c324da5bf1df6ccc617a902?version=3.29.7"},
         {id:"Docs", url : "https://kit.svelte.dev/docs/load#making-fetch-requests"},
-        {id:"Tutorial", url : "https://learn.svelte.dev/tutorial/each-blocks"}
+        {id:"Tutorial", url : "https://learn.svelte.dev/tutorial/each-blocks"},
+        {id:"Code Examples", url:"https://100joursdezbeul.fr/"},
     ];
+
+    const items = [
+        {id:"UI Library", url:"https://madewithsvelte.com/ui-library?page=3"},
+        {id:"Shadcn-svelte", url:"https://www.shadcn-svelte.com/"},
+        {id:"Spaper", url:"https://oli8.github.io/spaper/?ref=madewithsvelte.com#"},
+        {id:"Smelte", url:"https://smeltejs.com/"},
+        {id:"Grid.js", url:"https://gridjs.io/"},
+        {id:"STWUI", url:"https://stwui.vercel.app/"},
+        {id:"STDF", url:"https://stdf.design/?ref=madewithsvelte.com#/"},
+        {id:"Svelte UX", url:"https://svelte-ux.techniq.dev/"},
+        {id:"DaisyUI", url:"https://daisyui.com/"},
+        {id:"MeltUI", url:"https://melt-ui.com/"},
+        {id:"Skeleton", url:"https://www.skeleton.dev/"},
+        {id:"Flowbite Svelte", url:"https://flowbite-svelte.com/"},
+    ];
+
+    let openRow;
+    let details;
+    let doubleClickModal = false;
+
+    const toggleRow = (i) => {
+        openRow = openRow === i ? null : i
+    }
 
     // let groupedResourcesList=Object.groupBy(resourcesList, (site) => {return });
 
@@ -55,6 +91,40 @@
 
 <!-- <button on:click={handleClick}>fetch from api</button> -->
 <h1>{JSON.stringify(data.item.message, null, 2)}</h1>
+
+<Table color="red" hoverable={true}>
+    <TableHead>
+        <TableHeadCell>ID</TableHeadCell>
+        <TableHeadCell>URL</TableHeadCell>
+        <!-- <TableHeadCell>Category</TableHeadCell>
+        <TableHeadCell>Price</TableHeadCell> -->
+    </TableHead>
+    <TableBody tableBodyClass="divide-y">
+        {#each items as item, i}
+            <TableBodyRow on:click={() => toggleRow(i)}>
+                <TableBodyCell>{item.id}</TableBodyCell>
+                <TableBodyCell>{item.url}</TableBodyCell>
+                <!-- <TableBodyCell>{item.type}</TableBodyCell>
+                <TableBodyCell>{item.price}</TableBodyCell> -->
+            </TableBodyRow>
+            {#if openRow === i}
+                <TableBodyRow on:dblclick={() => {
+                    doubleClickModal = true;
+                    details = item;
+                    }}>
+                    <TableBodyCell colspan="4" class="p-0">
+                        <div class="px-2 py-3" transition:slide={{ duration: 300, axis: 'y' }}>
+                            <ImagePlaceholder />
+                        </div>
+                    </TableBodyCell>
+                </TableBodyRow>
+            {/if}
+        {/each}
+    </TableBody>
+</Table>
+<Modal title={details?.name} bind:open={doubleClickModal} autoclose outsideclose>
+    <ImagePlaceholder />
+</Modal>
 
 <ul>
 	{#each resourcesList as { id, url }, i}
